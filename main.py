40e396e9f3080b1f2411d6e95a61bbd7f7571228
AlexandrN <nikitinalexandr1888@yandex.ru>
2023-12-31 13:14:50 +0300

    Add grade_Lecturer

--- main.py
+++ main.py
@@ -7,12 +7,31 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_Lecturer(self, lecturer, course, grade_Lecturer):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade_Lecturer]
+            else:
+                lecturer.grades[course] = [grade_Lecturer]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades_Lecturer = {}
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -23,13 +42,6 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
